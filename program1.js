07a541a1b66170663982b4e235a439d71f8862ec
priyanshu01chouhan <ps9166993@gmail.com>
2024-09-30 15:35:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,20 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+// program1.js
+function isValid(s) {
+    let stack = [];
+    let matchingBracket = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (char in matchingBracket) {
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+            if (matchingBracket[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -1,10 +1,31 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+// program2.js
+function romanToInt(s) {
+    // Define Roman numeral values
+    const romanMap = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    };
 
+    let total = 0;
+    let prevValue = 0;
 
-module.exports={romanToInt}
+    // Traverse the string from right to left
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+
+        // If the current value is less than the previous value, subtract it (subtractive notation)
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            // Otherwise, add it
+            total += currentValue;
+        }
+
+        // Update the previous value
+        prevValue = currentValue;
+    }
+
+    return total;
+}
+
+module.exports = { romanToInt };
